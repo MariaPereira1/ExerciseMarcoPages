823c962490a7b49655885dba19875733c2d71b2d
mariapereira <mariapereira5a20@gmail.com>
2020-04-22 10:13:52 +0100

    Start login Page

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,28 @@
-import React, { useState, useLayoutEffect, useEffect } from "react";
-import { Link, withRouter } from "react-router-dom";
+import React from "react";
+import { NavLink, withRouter } from "react-router-dom";
 import "./styles.less";
 
-const Header = (props) => {
-  const [location, setLocation] = useState(props.location.pathname);
-
-  useEffect(() => setLocation(props.location.pathname), [
-    props.location.pathname
-  ]);
-
-  useLayoutEffect(() => {
-    const a = document.querySelector(".selected");
-    const b = document.querySelector(".line ");
-    if (a?.offsetWidth && b?.style && a?.offsetLeft) {
-      b.style.left = `${a.offsetLeft}px`;
-      b.style.width = `${a.offsetWidth}px`;
-    }
-  });
-
+const Header = ({ history }) => {
   return (
-    <div className="navContainer">
-      <div className="line"> </div>
-      <div className="nav">
-        <Link className={location === "/" ? "selected" : null} to="/">
+    <div className="header">
+      <nav className="nav">
+        <NavLink exact activeClassName="selected" to="/">
           Home
-        </Link>
-        <Link className={location === "/about" ? "selected" : null} to="/about">
+        </NavLink>
+        <NavLink activeClassName="selected" to="/about">
           About
-        </Link>
-        <Link
-          className={location === "/contact" ? "selected" : null}
-          to="/contact"
-        >
+        </NavLink>
+        <NavLink activeClassName="selected" to="/contact">
           Contact
-        </Link>
-        <Link
-          className={location === "/gallery" ? "selected" : null}
-          to="/gallery"
-        >
+        </NavLink>
+        <NavLink activeClassName="selected" to="/gallery">
           Gallery
-        </Link>
+        </NavLink>
+      </nav>
+      <div className="user">
+        <button className="login-button" onClick={() => history.push("/login")}>
+          Login
+        </button>
       </div>
     </div>
   );
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,17 +5,30 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Gallery from "../pages/Gallery";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
 import Header from "../components/Header";
 
 const Routes = () => (
   <BrowserRouter>
     <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/gallery" component={Gallery} />
-    </Switch>
+    <main
+      style={{
+        display: "flex",
+        flexGrow: 1,
+        flexDirection: "column",
+        alignItems: "center",
+        height: "100%",
+        width: "100%"
+      }}
+    >
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/login" component={Login} />
+      </Switch>
+    </main>
   </BrowserRouter>
 );
 
